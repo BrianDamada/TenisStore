4af07dc0e841c8f2b1457f5cdbcb3f600ed0c6ff
BrianDamada <114277605+BrianDamada@users.noreply.github.com>
2024-11-08 04:17:09 -0300

    Discount Erro (resolved)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
-import Discount from "../discount"
+import Discount from "../Discount";
+
+Discount;
 
 /* eslint-disable react/prop-types */
 
-export const Header = ({title, desconto}) => {
+export const Header = ({ title, desconto }) => {
   return (
-    <header >
-      <Discount desconto={desconto}/>
+    <header>
+      <Discount desconto={desconto} />
       <div className="p-5 bg-slate-100 flex w-full items-center justify-center">
         <h1 className="text-slate-900 text-3xl font-bold">{title}</h1>
       </div>
     </header>
-  )
-}
+  );
+};
